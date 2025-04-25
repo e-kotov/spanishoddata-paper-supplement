fa6d1350ded99dbcbcabcd07ebdcd09ed1d7d812
Egor Kotov <kotov.egor@gmail.com>
2025-04-25 17:56:08 +0200

    update Madrid plot

--- R/main/madrid-plot.R
+++ R/main/madrid-plot.R
@@ -1,4 +1,3 @@
-# Case Study 2: Exploring Work vs Non-work related trips in Madrid
 case_study_madrid_work_v_nonwork_trips_plot <- function(
   madrid_data_for_plots,
   plots_output_dir = "plots/main-plots"
@@ -8,7 +7,7 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
   zones_selected_fua <- madrid_data_for_plots$zones_selected_fua
   nodes <- madrid_data_for_plots$nodes
 
-  # create the base plot with Madrid districts
+  # Create base map
   base_plot_districts <- ggplot() +
     geom_sf(
       data = zones_selected_fua |> st_transform(4326),
@@ -28,9 +27,8 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
       panel.grid.minor = element_blank()
     ) +
     guides(fill = "none")
-  base_plot_districts
 
-  # aggregate flows for flowmapper
+  # Group OD flows
   od_home_work_study_morning <- od_trip_types_morning |>
     filter(
       activity_origin %in%
@@ -40,11 +38,18 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
     group_by(o, d) |>
     summarise(value = sum(value, na.rm = TRUE), .groups = "drop")
 
-  od_non_work_morning <- od_trip_types_morning |>
+  od_frequent_morning <- od_trip_types_morning |>
     filter(
-      activity_origin %in%
-        c("frequent_activity", "infrequent_activity") |
-        activity_destination %in% c("frequent_activity", "infrequent_activity")
+      activity_origin == "frequent_activity" |
+        activity_destination == "frequent_activity"
+    ) |>
+    group_by(o, d) |>
+    summarise(value = sum(value, na.rm = TRUE), .groups = "drop")
+
+  od_infrequent_morning <- od_trip_types_morning |>
+    filter(
+      activity_origin == "infrequent_activity" |
+        activity_destination == "infrequent_activity"
     ) |>
     group_by(o, d) |>
     summarise(value = sum(value, na.rm = TRUE), .groups = "drop")
@@ -58,22 +63,25 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
     group_by(o, d) |>
     summarise(value = sum(value, na.rm = TRUE), .groups = "drop")
 
-  od_non_work_evening <- od_trip_types_evening |>
+  od_frequent_evening <- od_trip_types_evening |>
     filter(
-      activity_origin %in%
-        c("frequent_activity", "infrequent_activity") |
-        activity_destination %in% c("frequent_activity", "infrequent_activity")
+      activity_origin == "frequent_activity" |
+        activity_destination == "frequent_activity"
     ) |>
     group_by(o, d) |>
     summarise(value = sum(value, na.rm = TRUE), .groups = "drop")
 
-  create_flowmap <- function(
-    od,
-    nodes,
-    edge_width_factor = 1
-  ) {
-    # add the flows to the basemap
-    flowmap_plot <- base_plot_districts |>
+  od_infrequent_evening <- od_trip_types_evening |>
+    filter(
+      activity_origin == "infrequent_activity" |
+        activity_destination == "infrequent_activity"
+    ) |>
+    group_by(o, d) |>
+    summarise(value = sum(value, na.rm = TRUE), .groups = "drop")
+
+  # Flowmap builder
+  create_flowmap <- function(od, nodes, edge_width_factor = 1) {
+    base_plot_districts |>
       flowmapper::add_flowmap(
         od = od,
         nodes = nodes,
@@ -85,15 +93,12 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
         add_legend = "bottom",
         legend_col = "gray20",
         legend_gradient = TRUE,
-        k_node = 20 # play around with this parameter to aggregate nodes and flows
-      )
-
-    # customise colours for the fill
-    flowmap_plot <- flowmap_plot +
+        k_node = 20
+      ) +
       scale_fill_gradient(
         low = "#FABB29",
         high = "#AB061F",
-        labels = scales::comma_format() # Real value labels
+        labels = scales::comma_format()
       ) +
       theme(
         axis.line = element_blank(),
@@ -102,60 +107,77 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
         axis.title = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        plot.title = element_text(size = 18, face = "bold"),
-        plot.subtitle = element_text(size = 16, face = "bold"),
+        plot.title = element_text(size = 14, face = "bold"),
+        plot.subtitle = element_text(size = 14, face = "bold")
       )
-
-    return(flowmap_plot)
   }
 
-  # `edge_width_factor` is adjusted after first plot attempt to normalise the flows legends, as flowmapper does not allow to change the width of the edges natively and facet the plots automatically
-
+  # Plots with adjusted width factors
   p_home_work_study_morning <- create_flowmap(
-    od = od_home_work_study_morning,
-    nodes = nodes,
-    edge_width_factor = 1 / 3.11
+    od_home_work_study_morning,
+    nodes,
+    edge_width_factor = 1 / 2.77
   ) +
-    labs(title = "A: Home <-> Work/Study", subtitle = "Morning (7-11) trips")
+    labs(title = "A:\nHome <-> Work/Study", subtitle = "Morning (7–11) trips")
 
-  p_non_work_morning <- create_flowmap(
-    od = od_non_work_morning,
-    nodes = nodes,
+  p_frequent_morning <- create_flowmap(
+    od_frequent_morning,
+    nodes,
     edge_width_factor = 1
   ) +
     labs(
-      title = "B: Home/Work/Study <-> Other",
-      subtitle = "Morning (7-11) trips"
+      title = "B:\nHome/Work/Study <-> Frequent activity",
+      subtitle = "Morning (7–11) trips"
+    )
+
+  p_infrequent_morning <- create_flowmap(
+    od_infrequent_morning,
+    nodes,
+    edge_width_factor = 1 / 2.42
+  ) +
+    labs(
+      title = "C:\nHome/Work/Study <-> Infrequent activity",
+      subtitle = "Morning (7–11) trips"
     )
 
   p_home_work_study_evening <- create_flowmap(
-    od = od_home_work_study_evening,
-    nodes = nodes,
-    edge_width_factor = 1 / 4.62
+    od_home_work_study_evening,
+    nodes,
+    edge_width_factor = 1 / 4.13
   ) +
-    labs(title = "C: Home <-> Work/Study", subtitle = "Evening (17-21) trips")
+    labs(title = "D:\nHome <-> Work/Study", subtitle = "Evening (17–21) trips")
 
-  p_non_work_evening <- create_flowmap(
-    od = od_non_work_evening,
-    nodes = nodes,
-    edge_width_factor = 1 / 1.19
+  p_frequent_evening <- create_flowmap(
+    od_frequent_evening,
+    nodes,
+    edge_width_factor = 1 / 1.07
   ) +
     labs(
-      title = "D: Home/Work/Study <-> Other",
-      subtitle = "Evening (17-21) trips"
+      title = "E:\nHome/Work/Study <-> Frequent activity",
+      subtitle = "Evening (17–21) trips"
     )
 
-  p_compare <- (p_home_work_study_morning + p_non_work_morning) /
-    (p_home_work_study_evening + p_non_work_evening)
+  p_infrequent_evening <- create_flowmap(
+    od_infrequent_evening,
+    nodes,
+    edge_width_factor = 1 / 1.83
+  ) +
+    labs(
+      title = "F:\nHome/Work/Study <-> Infrequent activity",
+      subtitle = "Evening (17–21) trips"
+    )
 
-  p_compare <- p_compare +
+  # Combine into 6-panel plot
+  p_compare <- (p_home_work_study_morning +
+    p_frequent_morning +
+    p_infrequent_morning) /
+    (p_home_work_study_evening + p_frequent_evening + p_infrequent_evening) +
     plot_annotation(
-      caption = "Mobility flows in Madrid area\nMean over a week of February 6-10, 2023",
-      theme = theme(
-        plot.caption = element_text(size = 12),
-      )
+      caption = "Mobility flows in Madrid area\nMean over a week of February 6–10, 2023",
+      theme = theme(plot.caption = element_text(size = 12))
     )
 
+  # Save
   if (!fs::dir_exists(plots_output_dir)) {
     fs::dir_create(plots_output_dir)
   }
@@ -165,8 +187,8 @@ case_study_madrid_work_v_nonwork_trips_plot <- function(
   ggsave(
     plot_save_path,
     plot = p_compare,
-    width = 9,
-    height = 12,
+    width = 12,
+    height = 14,
     units = "in",
     dpi = 300,
     create.dir = TRUE
